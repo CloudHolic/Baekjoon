dc875cd9c0e13a6f65bdaf6071751b20c3ff296f
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-09-17 21:15:17 +0900

    Add 2373, 2862

---
+++ 2862.fsx
@@ -0,0 +1,16 @@
+open System
+
+[<EntryPoint>]
+let main _ =
+    let fibSeq : int64 seq = Seq.unfold (fun (a, b) -> Some(a + b, (b, a + b))) (int64 0, int64 1)
+    let fibList (n:int64) = fibSeq |> Seq.takeWhile (fun x -> x <= n) |> Seq.toList |> List.rev
+
+    let size = Console.ReadLine() |> int64
+
+    let rec solve (n:int64) (fiblist: int64 list) =
+        match (n, List.head fiblist) with
+        | (n, h) when n = h -> n
+        | _ -> let newSize = n - List.head fiblist in (newSize, List.filter (fun x -> x <= newSize) fiblist) ||> solve
+    
+    printfn "%d" <| (solve size <| fibList size)
+    0
