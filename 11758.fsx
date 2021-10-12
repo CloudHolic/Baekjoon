cf446ae43da7f4c53097ff79b3f1e62e88dc7b74
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-10-12 17:18:01 +0900

    Add 11758

---
+++ 11758.fsx
@@ -0,0 +1,20 @@
+open System
+
+[<EntryPoint>]
+let main argv =
+    let makePoint = Array.map int
+    let makeVec = Array.map2 (fun x y -> x - y)
+
+    let first = Console.ReadLine().Split() |> makePoint
+    let second = Console.ReadLine().Split() |> makePoint
+    let third = Console.ReadLine().Split() |> makePoint
+
+    let vec1 = makeVec second first
+    let vec2 = makeVec third second
+    
+    let ccw = vec1.[0] * vec2.[1] - vec1.[1] * vec2.[0]
+    match ccw with
+    | c when c < 0 -> printfn "-1"
+    | c when c > 0 -> printfn "1"
+    | _ -> printfn "0"
+    0
