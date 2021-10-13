6e5891a13bb026b00af8b994549df0170176c0b7
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-10-13 12:15:29 +0900

    Modify 12781(Add trim)

--- 12781.fsx
+++ 12781.fsx
@@ -9,9 +9,9 @@ let main _ =
         | 1 -> b
         | 2 -> c
         | 3 -> d
-        | _ -> failwith("Unpack failed - Invalid index.")
+        | _ -> failwith("Unpack failed - Invalid index.")       
 
-    let makePoints = Array.map int    
+    let makePoints = Array.map int
     let splitPoint (a: int []) = (a.[0 .. 1], a.[2 .. 3], a.[4 .. 5], a.[6 .. 7])
     let ccw a b c =
         let makeVec = Array.map2 (fun x y -> x - y)
@@ -23,13 +23,15 @@ let main _ =
             | c when c > 0 -> 1
             | _ -> 0
 
-    let points = Console.ReadLine().Split() |> makePoints |> splitPoint
+    let points = Console.ReadLine().Trim().Split() |> makePoints |> splitPoint
     
     let abc = (unpack points 0, unpack points 1, unpack points 2) |||> ccw
     let abd = (unpack points 0, unpack points 1, unpack points 3) |||> ccw
+    let cda = (unpack points 2, unpack points 3, unpack points 0) |||> ccw
+    let cdb = (unpack points 2, unpack points 3, unpack points 1) |||> ccw
 
-    abc * abd
+    (abc * abd, cda * cdb)
     |> function
-        | c when c < 0 -> printfn "1"
+        | (c1, c2) when c1 < 0 && c2 < 0 -> printfn "1"
         | _ -> printfn "0"
     0
