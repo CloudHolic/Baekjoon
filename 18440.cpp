c21a7c65667d5860dca3f3211a267d31d756558a
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-10-22 22:25:21 +0900

    Modify 18440

--- 18440.cpp
+++ 18440.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
-#include <cstring>
+#include <vector>
 
-using namespace std;
+#pragma warning (disable: 4554)
+#pragma warning (disable: 26451)
 
 using namespace std;
 
@@ -13,9 +14,7 @@ typedef unsigned long long uint64;
 
 string str_a, str_b;
 string answer;
-int len_a, len_b;
 
-uint64 cache_f[782], cache_b[782], match[26][782], r_match[26][782];
 short result_f[50001], result_b[50001];
 
 uint64 subtract(uint64& x, const uint64 y)
@@ -40,25 +39,30 @@ void solve(const int start_a, const int end_a, const int start_b, const int end_
 {
     // 1. Initialize & Split str_a in half
     const int mid = (start_a + end_a) / 2;
-    const int start_block = start_b >> 6, end_block = end_b >> 6;
+    int len_a = 0, len_b = end_b - start_b + 1;
+    const int block_size = (len_b >> 6) + 1;
 
-    memset(cache_f + start_b - 1, 0, sizeof(short) * (end_b - start_b + 2));
-    memset(cache_b + start_b, 0, sizeof(short) * (end_b - start_b + 2));
+    vector<uint64> cache_f(block_size, 0), cache_b(block_size, 0);
+    vector<vector<uint64>> match(26, vector<uint64>(block_size, 0));
+	vector<vector<uint64>> r_match(26, vector<uint64>(block_size, 0));
 
-    // 2. Forwarding DP    
-    string str_a_f = str_a.substr(start_a, mid - start_a);
-    string str_b_f = str_b.substr(start_b, end_b - start_b);
+    result_f[start_b - 1] = result_b[end_b + 1] = 0;
+    
+    // 2. Forwarding DP
+    string str_a_f = str_a.substr(start_a - 1, mid - start_a + 1);
+    string str_b_f = str_b.substr(start_b - 1, len_b);
+    len_a = static_cast<int>(str_a_f.size());
 
-    for (int i = 0; i < str_b_f.size(); i++)
+    for (int i = 0; i < len_b; i++)
         set_bit(match[str_b_f[i] - 'A'], i);
-
-    for (int i = start_a; i <= mid; i++)
+    
+    for (int i = 0; i < len_a; i++)
     {
         uint64 shift_carry = 1, subtract_carry = 0;
 
-        for (int k = start_block; k <= end_block; k++)
+        for (int k = 0; k < block_size; k++)
         {
-            uint64 temp1 = match[str_a[i - 1] - 'A'][k] >> start_b - 1 | cache_f[k];
+            uint64 temp1 = match[str_a_f[i] - 'A'][k] | cache_f[k];
 
             const uint64 temp2 = cache_f[k] << 1 | shift_carry;
             shift_carry = cache_f[k] >> 63;
@@ -71,58 +75,69 @@ void solve(const int start_a, const int end_a, const int start_b, const int end_
         }
     }
 
-    for (int i = start_a; i <= mid; i++)
-        for (int j = start_b; j <= end_b; j++)
-            if (str_a[i - 1] == str_b[j - 1])
-                cache_f[i & 1][j] = cache_f[i & 1 ^ 1][j - 1] + 1;
-            else
-                cache_f[i & 1][j] = max(cache_f[i & 1 ^ 1][j], cache_f[i & 1][j - 1]);
+    for (int i = start_b; i <= end_b; i++)
+        result_f[i] = result_f[i - 1] + get_bit(cache_f, i - start_b);
 
     // 3. Reverse DP
-    string str_a_r = str_a.substr(start_a, end_a - mid);
-    string str_b_r = str_b.substr(start_b, end_b - start_b);
+    string str_a_r = str_a.substr(mid, end_a - mid);
+    string str_b_r = str_b.substr(start_b - 1, len_b);
     reverse(str_a_r.begin(), str_a_r.end());
     reverse(str_b_r.begin(), str_b_r.end());
-    
+    len_a = static_cast<int>(str_a_r.size());
 
-    for (int i = 0; i < str_b_r.size(); i++)
+    for (int i = 0; i < len_b; i++)
         set_bit(r_match[str_b_r[i] - 'A'], i);
+    
+    for (int i = 0; i < len_a; i++)
+    {
+        uint64 shift_carry = 1, subtract_carry = 0;
 
-    for (int i = end_a; i > mid; i--)
-        for (int j = end_b; j >= start_b; j--)
-            if (str_a[i - 1] == str_b[j - 1])
-                cache_b[i & 1][j] = cache_b[i & 1 ^ 1][j + 1] + 1;
-            else
-                cache_b[i & 1][j] = max(cache_b[i & 1 ^ 1][j], cache_b[i & 1][j + 1]);
+        for (int k = 0; k < block_size; k++)
+        {
+            uint64 temp1 = r_match[str_a_r[i] - 'A'][k] | cache_b[k];
+
+            const uint64 temp2 = cache_b[k] << 1 | shift_carry;
+            shift_carry = cache_b[k] >> 63;
+
+            uint64 temp3 = temp1;
+            subtract_carry = subtract(temp3, subtract_carry);
+            subtract_carry += subtract(temp3, temp2);
+
+            cache_b[k] = temp1 ^ temp1 & temp3;
+        }
+    }
+
+    for (int i = end_b; i >= start_b; i--)
+        result_b[i] = result_b[i + 1] + get_bit(cache_b, end_b - i);
 
     // 4. Find maximum index & answer
     int idx, max;
 
-    if (cache_f[mid & 1][end_b] > cache_b[mid & 1 ^ 1][start_b])
+    if (result_f[end_b] > result_b[start_b])
     {
         idx = end_b;
-        max = cache_f[mid & 1][end_b];
+        max = result_f[end_b];
     }
     else
     {
         idx = start_b - 1;
-        max = cache_b[mid & 1][start_b];
+        max = result_b[start_b];
     }
 
     for (int i = start_b; i < end_b; i++)
     {
-        const int cur = cache_f[mid & 1][i] + cache_b[mid & 1 ^ 1][i + 1];
+        const int cur = result_f[i] + result_b[i + 1];
         if (cur <= max)
             continue;
 
         max = cur;
         idx = i;
     }
-
+    
     // 5. Check & record the answer and split into two problems.
     if (mid == start_a)
     {
-        if (cache_f[mid & 1][idx] > 0)
+        if (result_f[idx] > 0)
             answer.push_back(str_a[start_a - 1]);
     }
     else
@@ -138,7 +153,7 @@ int main()
     cin.tie(nullptr);
 
     cin >> str_a >> str_b;
-    solve(0, static_cast<int>(str_a.length() - 1), 0, static_cast<int>(str_b.length() - 1));
+    solve(1, static_cast<int>(str_a.length()), 1, static_cast<int>(str_b.length()));
     cout << answer.length() << "\n" << answer;
     return 0;
 }
