b3b97073fbafd1d3bd84a5e3ba61460028522698
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-11-08 13:16:18 +0900

    Add 2751

---
+++ 02. Silver/2751.fsx
@@ -0,0 +1,21 @@
+open System
+open System.IO
+open System.Text
+
+[<EntryPoint>]
+let main _ =
+    use stream = new StreamReader(Console.OpenStandardInput())
+    let result = new StringBuilder()
+
+    let size = stream.ReadLine() |> int
+    let arr = Array.zeroCreate size
+
+    for i in 1 .. size do
+        arr.[i - 1] <- stream.ReadLine() |> int
+
+    arr
+    |> Array.sort
+    |> Array.iter (fun x -> result.AppendFormat("{0}\n", x) |> ignore)
+
+    printfn "%A" result
+    0
