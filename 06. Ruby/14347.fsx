cf243c973a0b2e9ea4426822333305d9580c3e79
CloudHolic <bluegkstjr@kaist.ac.kr>
2022-01-10 23:31:08 +0900

    Modify 14347

--- 06. Ruby/14347.fsx
+++ 06. Ruby/14347.fsx
@@ -2,8 +2,6 @@ open System
 open System.IO
 open System.Text
 
-let inline (||||>) (a, b, c, d) f = f a b c d
-
 [<EntryPoint>]
 let main _ =
     use stream = new StreamReader(Console.OpenStandardInput())
@@ -21,7 +19,7 @@ let main _ =
                 let temp3 = x * x + y * y
                 temp1 * temp2 / (temp3 * (temp3 + 1.0))
                 
-            let mutable x, px = -10.0, 0.00001
+            let mutable x, px = -10.0, 0.0001
             let mutable y, py = a, py
             let mutable result = 0.0
 
@@ -36,7 +34,7 @@ let main _ =
             y, result
 
         let rec inner low high =
-            if low - high |> abs > 1e-7 then
+            if low - high |> abs > 1e-5 then
                 let mid = (low + high) / 2.0
                 let endpos, _ = trial mid
                 match endpos with
@@ -49,22 +47,21 @@ let main _ =
         |> List.map (fun x -> x ||> inner)
         |> List.min
 
-    let solve2 a b c1 c2 =
+    let solve2 a b c =
         let trial py =
             let get_length posx posy len =
-                let temp1 = posy - c1 |> abs
-                let temp2 = posy - c2 |> abs
-                len * (1.0 + 1.0 / (posx * posx + temp1 * temp1) + 1.0 / (posx * posx + temp2 * temp2))
+                let temp = posy - c
+                len * (1.0 + 1.0 / (posx * posx + posy * posy) + 1.0 / (posx * posx + temp * temp))
 
             let eval_ppy x y py =
                 let temp1 = 1.0 + py * py
-                let temp2 = x * x + (y - c1) * (y - c1)
-                let temp3 = x * x + (y - c2) * (y - c2)
-                let temp4 = -2.0 * (y - c1) / (temp2 * temp2)
-                let temp5 = -2.0 * (y - c2) / (temp3 * temp3)
-                temp1 * (temp4 + temp5) / (1.0 / temp2 + 1.0 / temp3 + 1.0)
+                let temp2 = x * x + y * y
+                let temp3 = x * x + (y - c) * (y - c)
+                let temp4 = -2.0 * y / (temp2 * temp2)
+                let temp5 = -2.0 * (y - c) / (temp3 * temp3)
+                temp1 * (temp4 + temp5) / (1.0 + 1.0 / temp2 + 1.0 / temp3)
         
-            let mutable x, px = -10.0, 0.00001
+            let mutable x, px = -10.0, 0.0001
             let mutable y, py = a, py
             let mutable result = 0.0
 
@@ -79,17 +76,16 @@ let main _ =
             y, result
 
         let rec inner low high =
-            if low - high |> abs > 1e-7 then
+            if low - high |> abs > 1e-5 then
                 let mid = (low + high) / 2.0
                 let endpos, _ = trial mid
                 match endpos with
                 | c when c < b -> inner mid high
                 | _ -> inner low mid
             else
-                let mid = (low + high) / 2.0
-                trial mid |> snd
+                trial low |> snd
 
-        [(-100.0, (a - c1) / 10.0); ((a - c1) / 10.0, (a - c2) / 10.0); ((a - c2) / 10.0, 100.0)]
+        [(-100.0, -a / 10.0); (-a / 10.0, (c - a) / 10.0); ((a - c) / 10.0, 100.0)]
         |> List.map (fun x -> x ||> inner)
         |> List.min
 
@@ -98,10 +94,10 @@ let main _ =
         match n with
         | 1 ->
             let c = stream.ReadLine().Trim() |> float
-            result.AppendFormat("Case #{0}: {1:N3}\n", i, (a - c, b - c) ||> solve1) |> ignore
+            result.AppendFormat("Case #{0}: {1:N2}\n", i, (a - c, b - c) ||> solve1) |> ignore
         | _ ->
-            let c1, c2 = stream.ReadLine().Trim().Split() |> Array.map float |> function | c -> c[0], c[1]
-            result.AppendFormat("Case #{0}: {1:N3}\n", i, (a, b, min c1 c2, max c1 c2) ||||> solve2) |> ignore
+            let c1, c2 = stream.ReadLine().Trim().Split() |> Array.map float |> function | c -> Array.min c, Array.max c
+            result.AppendFormat("Case #{0}: {1:N2}\n", i, (a - c1, b - c1, c2 - c1) |||> solve2) |> ignore
 
     printfn "%A" result
     0
